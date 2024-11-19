77d509036eba3330f6080deb48d6fe9959401ebd
Thomas Rains <thomasrains01@gmail.com>
2024-11-19 11:49:49 -0800

    submits target sum lab

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-function hasTargetSum(array, target) {
+//function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  //This will take an array, sort through possible combinatinations,
+  //and return true if any pair equals the target value
+//}
 
 /* 
   Write the Big O time complexity of your function here
+  //I will be attempting to use a nested loop, which would be O(n²)
 */
 
 /* 
   Add your pseudocode here
 */
-
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] + array[i] === target) 
+        return true;
+    }
+  }
+  return false;
+}
 /*
   Add written explanation of your solution here
 */
